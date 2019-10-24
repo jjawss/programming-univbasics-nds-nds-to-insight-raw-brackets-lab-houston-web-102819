ff423add4c6392e477316388f5e35ec66829b1f0
David Jaworski <jaws360@gmail.com>
2019-10-24 23:13:17 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
@@ -7,8 +8,8 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
+  #result = {
+  #}
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
@@ -18,5 +19,42 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+ 
+ #hash = {
+  #directors_database[0][:name] => directors[database][0 #static][:movies][0 #loop][+=:worldwide_gross] 
+  
+  row_index = 0
+  result = {}
+  while row_index < nds.length
+    director = nds[row_index][:name]
+    column_index = 0
+    total = 0
+    while column_index < nds[row_index][:movies].length
+      total += nds[row_index][:movies][column_index][:worldwide_gross]
+      column_index += 1
+    end
+    row_index += 1
+    result[director] = total
+  end
+  puts result
+  return result
 end
+
+directors_totals(directors_database)
+
+
+#[{:name=>"Stephen Spielberg",
+  #:movies=>
+   #[{:title=>"Jaws",
+     #:studio=>"Universal",
+     #:worldwide_gross=>260000000,
+     #:release_year=>1975},
+    #{:title=>"Close Encounters of the Third Kind",
+     #:studio=>"Columbia",
+     #:worldwide_gross=>135189114,
+     #:release_year=>1977},
+    #{:title=>"Raiders of the Lost Ark",
+     #:studio=>"Paramount",
+     #:worldwide_gross=>248159971,
+     #:release_year=>1981},
+
